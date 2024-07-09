eee1e786ca5d6e066bc10b91c95951eb8c59fe66
Olaf Chwołka <olaf.chwolka@outlook.com>
2024-07-10 00:38:24 +0200

    feat: ended game

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,68 @@
 import "./App.css";
 import { HangImage } from "./components/HangImage";
 import { letters } from "./helpers/letters";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { getRandomWord } from "./helpers/getRandomWord";
 
 export function App() {
 	const [attempts, setAttempts] = useState(0);
-
-	const word = "COMPUTER";
+	const [word, setWord] = useState(getRandomWord());
+	const [hiddenWord, setHiddenWord] = useState("_ ".repeat(word.length).trim());
+	const [lose, setLose] = useState(false);
+	const [won, setWon] = useState(false);
 
 	const checkLetter = (letter: string) => {
-		console.log(letter);
-		setAttempts(Math.min(attempts + 1, 9));
+		if (lose) return;
+		if (won) return;
+		if (!word.includes(letter)) {
+			setAttempts(Math.min(attempts + 1, 9));
+			return;
+		}
+		const hiddenWordArray = hiddenWord.split(" ");
+		for (let i = 0; i < word.length; i++) {
+			if (word[i] === letter) {
+				hiddenWordArray[i] = letter;
+			}
+		}
+		setHiddenWord(hiddenWordArray.join(" "));
 	};
+
+	const newGame = () => {
+		const newWord = getRandomWord();
+		setWord(newWord);
+		setAttempts(0);
+		setHiddenWord("_ ".repeat(newWord.length).trim());
+		setLose(false);
+		setWon(false);
+	};
+
+	useEffect(() => {
+		if (attempts === 9) {
+			setLose(true);
+		}
+	}, [attempts]);
+
+	useEffect(() => {
+		const currentHiddenWord = hiddenWord.split(" ").join("");
+		if (currentHiddenWord === word) {
+			setWon(true);
+		}
+	}, [hiddenWord]);
 
 	return (
 		<div className='App'>
 			<HangImage imageNumber={attempts} />
-			<h2>_ _ _ _ _ _ _ _ _ _ _ _ _ _ _</h2>
+			<h2>{hiddenWord}</h2>
 			<h3>Attempts: {attempts}</h3>
+			{lose ? <h2>You've lost... The correct word is {word}</h2> : ""}
+			{won ? <h2>Winner! The word is {word}</h2> : ""}
 			{letters.map(letter => (
-				<button
-					style={{ backgroundColor: "lightpink" }}
-					key={letter}
-					onClick={() => checkLetter(letter)}
-				>
+				<button key={letter} onClick={() => checkLetter(letter)}>
 					{letter}
 				</button>
 			))}
+			<br />
+			<button onClick={newGame}>New game</button>
 		</div>
 	);
 }
