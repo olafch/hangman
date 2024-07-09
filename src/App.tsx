3fe9ea57a375116474260447343bb5d7ff9a3b24
Olaf Chwołka <olaf.chwolka@outlook.com>
2024-07-09 20:52:17 +0200

    feat: draft done

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,10 @@ import "./App.css";
 export function App() {
 	return (
 		<div className='App'>
-			<h1>Hola mundo!!!</h1>
+			<h1>Game's image</h1>
+			<h1>_ _ _ _ _ _ _ _ _ _ _ _ _ _ _</h1>
+			<h1>Attempts: 0</h1>
+			<h1>A B C D E F ... X Y Z</h1>
 		</div>
 	);
 }
